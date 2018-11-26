d58562f2d24e04db31d5e1c6177b3ed3ff3bbb32
Chris Burkhardt <chris.burkhardt@x-team.com>
2018-11-26 15:46:28 +0900

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import './QuizQuestion.js'
+import QuizQuestion from "./QuizQuestion";
 
 let quizData = require('./quiz_data.json');
 
@@ -7,12 +9,11 @@ class Quiz extends Component {
     super(props);
     this.state = {quiz_position: 1};
   }
+  // send data for the first question over to the QuizQuestion Component
   render () {
     return (
       <div>
-        <div className="QuizQuestion">
-          {quizData.quiz_questions[0].instruction_text}
-        </div>
+        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
       </div>
     );
   };
